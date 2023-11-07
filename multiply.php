6563b6fcdd514c23a5af099292844b10781159cc
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-11-07 14:53:27 -0600

    Update multiply.php

--- multiply.php
+++ multiply.php
@@ -1,1 +1,24 @@
+<?php
+$pageTitle = "Multiply";
+include "view-header.php";
+?>
+
+<html>
+      <head>
+          <title>Multiplication Calculator</title>
+      </head>
+      <body>
+          <h1>Addition Calculator</h1>
+          <input type="number" id="num1" placeholder="Enter first number">
+          
+          <input type="number" id="num2" placeholder="Enter second number">
+          <button onclick="getMultiply()">Calculate</button>
+          <p>Result: <span id="result"></span></p>
+          <script src="script.js"></script>
+      </body>
+</html>
+
+<?php
+include "view-footer.php";
+?>
 
