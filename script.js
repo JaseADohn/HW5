7ef0f1f9f9a5cb576089a588f1ede0740a2b150c
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-11-07 14:09:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ function calculateSum() {
         document.getElementById('result').textContent = 'Please enter valid numbers';
     }
 }
+
+function generateRandomNumber() {
+    const min = parseInt(prompt('Enter the minimum value:'));
+    const max = parseInt(prompt('Enter the maximum value:'));
+
+    if (!isNaN(min) && !isNaN(max)) {
+        const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+        document.getElementById('randomResult').textContent = `Random Number: ${randomNum}`;
+    } else {
+        document.getElementById('randomResult').textContent = 'Please enter valid minimum and maximum values.';
+    }
+}
