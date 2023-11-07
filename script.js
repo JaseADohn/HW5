7e7965e6d6f3156ed78c8a14c4e48c62f47170b8
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-11-07 14:11:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function calculateSum() {
+function getSum() {
     const num1 = parseFloat(document.getElementById('num1').value);
     const num2 = parseFloat(document.getElementById('num2').value);
 
