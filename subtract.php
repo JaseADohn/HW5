cf05065de33de965c773371f74de0de9dad81693
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-11-07 14:59:48 -0600

    Update subtract.php

--- subtract.php
+++ subtract.php
@@ -1,1 +1,24 @@
+<?php
+$pageTitle = "Subtraction";
+include "view-header.php";
+?>
+
+<html>
+      <head>
+          <title>Subtraction Calculator</title>
+      </head>
+      <body>
+          <h1>Subtraction Calculator</h1>
+          <input type="number" id="num1" placeholder="Enter first number">
+          
+          <input type="number" id="num2" placeholder="Enter second number">
+          <button onclick="getSubtract()">Calculate</button>
+          <p>Result: <span id="result"></span></p>
+          <script src="script.js"></script>
+      </body>
+</html>
+
+<?php
+include "view-footer.php";
+?>
 
