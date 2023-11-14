9365fc68bc40666a2d1690575d01fdec9c52f116
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-11-14 10:23:26 -0600

    Update view-footer.php

--- view-footer.php
+++ view-footer.php
@@ -1,4 +1,7 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="java.js"></script>
   </body>
 </html>
