c11086e866eb576e7909568596f13e41cd44f612
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-11-14 11:10:46 -0600

    Update divide.php

--- divide.php
+++ divide.php
@@ -63,6 +63,12 @@ include "view-header.php";
           <button id="divisionBtn">Calculate</button>
             <button id="clearBtn">Clear</button>
           <p>Result: <span id="result"></span></p>
+                <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+                <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+                <script src="https://cdn.jsdelivr.net/npm/mousetrap@1.6.5/mousetrap.min.js"></script>
+                <script src="java.js"></script>
+                <script src="https://cdn.jsdelivr.net/npm/moment@2.29.1/moment.min.js"></script>
+                <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
             <canvas id="resultChart" width="400" height="200"></canvas>
       </body>
 </html>
