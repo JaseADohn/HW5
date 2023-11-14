0729d92167ddf9a562d4594832818462e682e493
JaseADohn <112443263+JaseADohn@users.noreply.github.com>
2023-11-13 21:57:19 -0600

    Update java.js

--- java.js
+++ java.js
@@ -53,27 +53,4 @@ function clearEntries() {
     document.getElementById('result').textContent = '';
 }
 
-const ctx = document.getElementById('myChart').getContext('2d');
-const chartData = {
-    labels: ['Sum', 'Subtract', 'Divide', 'Multiply'],
-    datasets: [{
-        label: 'Operations Result',
-        data: [],
-        backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(255, 99, 132, 0.2)', 'rgba(255, 205, 86, 0.2)', 'rgba(54, 162, 235, 0.2)'],
-        borderColor: ['rgba(75, 192, 192, 1)', 'rgba(255, 99, 132, 1)', 'rgba(255, 205, 86, 1)', 'rgba(54, 162, 235, 1)'],
-        borderWidth: 1
-    }]
-};
-
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: chartData,
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
 
